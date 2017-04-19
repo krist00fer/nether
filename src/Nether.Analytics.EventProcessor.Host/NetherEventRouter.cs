5a5b3f55b7f4a56799618b16cbaeae421a6276e7
Kristofer Liljeblad <kristol@microsoft.com>
2017-04-19 18:46:39 +0200

    Created stub implementations of classes needed for a customizable Event Processor

---
+++ src/Nether.Analytics.EventProcessor.Host/NetherEventRouter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Nether.Analytics.EventProcessor.Host
+{
+    internal class NetherEventRouter
+    {
+        public NetherEventRouter()
+        {
+            GameEventHandlers = new Dictionary<string, object>();
+        }
+
+        public Dictionary<string, object> GameEventHandlers { get; internal set; }
+        public PassThroughGameEventHandler UnknownGameEventHandler { get; internal set; }
+    }
+}
