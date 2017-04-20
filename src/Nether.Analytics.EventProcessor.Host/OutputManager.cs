0b590042c89d5ece48f8f38b6ec05060033c95d0
Stuart Leeks <stuartle@microsoft.com>
2017-04-20 16:04:21 +0100

    More prototyping

--- src/Nether.Analytics.EventProcessor.Host/OutputManager.cs
+++ src/Nether.Analytics.EventProcessor.Host/OutputManager.cs
@@ -1,6 +1,11 @@
-﻿namespace Nether.Analytics.EventProcessor.Host
+﻿using System;
+
+namespace Nether.Analytics.EventProcessor.Host
 {
     internal class OutputManager
     {
+        internal void OutputMessage(GameMessage msg)
+        {
+        }
     }
 }
