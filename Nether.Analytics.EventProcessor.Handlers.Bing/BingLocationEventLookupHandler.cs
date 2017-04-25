7bbf1e572f62da51489e410a6e7f34b2f7928ec1
Kristofer Liljeblad <kristol@microsoft.com>
2017-04-25 14:41:29 +0200

    moved files to correct project

---
+++ Nether.Analytics.EventProcessor.Handlers.Bing/BingLocationEventLookupHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nether.Analytics.EventProcessor.Handlers.Bing
+{
+    public class BingLocationEventLookupHandler
+    {
+        public string HelloWorld()
+        {
+            return "Hello World!";
+        }
+    }
+}
